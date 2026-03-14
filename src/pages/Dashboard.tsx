b07defbe15799a42b62acca48ee21cdb80de8e4b
Harry Thomson <haroldt95@hotmail.com>
2026-03-14 19:36:50 +0900

    Implement a button that calls the dashboard endpoint

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,10 @@
+import { getDashboard } from '../api/dashboard'
+
 export default function Dashboard() {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <div>Dashboard</div>
+      <button onClick={getDashboard}>I am a button</button>
+    </>
+  )
 }

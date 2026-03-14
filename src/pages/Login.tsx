b93b881eb2892b7d7478bfae4cff69ad39d6d1fb
Harry Thomson <haroldt95@hotmail.com>
2026-03-14 13:32:36 +0900

    Create a default form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,24 @@
+import { useForm } from 'react-hook-form'
+import type { SubmitHandler } from 'react-hook-form'
+
+type Inputs = {
+  email: string
+  password: string
+}
+
 export default function Login() {
-  return <div>Login</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="test" {...register('email')} />
+      <input {...register('password', { required: true })} />
+      {errors.password && <span>This field is required</span>}
+      <input type="submit" />
+    </form>
+  )
 }

79adf9960ebe1160be2b75adf5a16194fae66bca
Harry Thomson <haroldt95@hotmail.com>
2026-03-14 10:55:34 +0900

    Install linter to run on commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
+      fdaf
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;

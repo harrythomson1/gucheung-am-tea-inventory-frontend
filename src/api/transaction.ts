bc6d0da1e0f3e915c93e26decc3bf668089e5901
Harry Thomson <haroldt95@hotmail.com>
2026-03-15 14:20:47 +0900

    Add a removal transaction method in the transaction api call

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -1,7 +1,19 @@
 import api from './api'
-import type { AddTransactionData } from '../types/transaction'
+import type {
+  AddTransactionData,
+  RemoveTransactionData,
+} from '../types/transaction'
 
-export const postHarvestTransaction = async (transaction_data: AddTransactionData) => {
+export const postHarvestTransaction = async (
+  transaction_data: AddTransactionData
+) => {
   const response = await api.post('/transactions', transaction_data)
+  return response.data
+}
+
+export const postRemovalTransaction = async (
+  transction_data: RemoveTransactionData
+) => {
+  const response = await api.post('/transactions', transction_data)
   return response.data
 }

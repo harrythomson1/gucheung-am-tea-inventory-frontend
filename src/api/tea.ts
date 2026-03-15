cb711188ae01c787b25cfc726713a9b1d82e4652
Harry Thomson <haroldt95@hotmail.com>
2026-03-15 15:32:55 +0900

    Create api endpoint to get tea stock

--- src/api/tea.ts
+++ src/api/tea.ts
@@ -4,3 +4,8 @@ export const getTeas = async () => {
   const response = await api.get('/teas')
   return response.data
 }
+
+export const getTeaStock = async (teaId: number) => {
+  const response = await api.get(`/teas/${teaId}/stock`)
+  return response.data
+}

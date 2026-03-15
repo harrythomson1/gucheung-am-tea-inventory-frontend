196e6e6745bd6642c68d4a9fb9c72e34fba6a40e
Harry Thomson <haroldt95@hotmail.com>
2026-03-15 10:03:32 +0900

    Create add stock form template

---
+++ src/components/AddStockForm.tsx
@@ -0,0 +1,13 @@
+type Inputs = {
+  tea_id: number
+  packaging: string
+  flush: string
+  harvest_year: number
+  weight_grams: number
+  quantity_change: number
+  notes: string | undefined
+}
+
+export default function AddStockForm() {
+  return <div>Add stock form</div>
+}

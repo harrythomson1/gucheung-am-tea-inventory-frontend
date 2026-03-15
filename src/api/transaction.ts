62ea711f9ad2c5e650733afef5a83f9adedc9791
Harry Thomson <haroldt95@hotmail.com>
2026-03-15 12:27:17 +0900

    Fix transaction endpoint to be transactions

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -2,6 +2,6 @@ import api from './api'
 import type { AddTransactionData } from '../types/transaction'
 
 export const postTransaction = async (transaction_data: AddTransactionData) => {
-  const response = await api.post('/transaction', transaction_data)
+  const response = await api.post('/transactions', transaction_data)
   return response.data
 }

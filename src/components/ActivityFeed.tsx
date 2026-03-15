69503f040ecf35e6a6330c65ec97fed83c4b4c40
Harry Thomson <haroldt95@hotmail.com>
2026-03-15 18:09:03 +0900

    Create activity feed type

---
+++ src/components/ActivityFeed.tsx
@@ -0,0 +1,14 @@
+export type ActivityFeedType = {
+  quantity_change: number
+  transaction_type: string
+  performed_by_name: string
+  buyer_name?: string
+  sales_channel?: string
+  notes?: string
+  packaging: string
+  flush: string
+  harvest_year: number
+  weight_grams: number
+  tea_name: string
+  created_at: string
+}

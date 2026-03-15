6abd355110914c9b57fdf1c631f7190086caa4ae
Harry Thomson <haroldt95@hotmail.com>
2026-03-15 10:44:58 +0900

    Create onSubmit function for form

--- src/components/AddStockForm.tsx
+++ src/components/AddStockForm.tsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react'
 import { getTeas } from '../api/tea'
+import type { SubmitHandler } from 'react-hook-form'
+import type { AddTransactionData } from '../types/transaction'
+import { postTransaction } from '../api/transaction'
+import { useNavigate } from 'react-router-dom'
 
-type Inputs = {
-  tea_id: number
-  packaging: string
-  flush: string
-  harvest_year: number
-  weight_grams: number
-  quantity_change: number
-  notes: string | undefined
-}
+type AddStockInputs = Omit<AddTransactionData, 'transaction_type'>
 
 export default function AddStockForm() {
+  const navigate = useNavigate()
   const [teas, setTeas] = useState([])
   useEffect(() => {
     getTeas().then((response) => setTeas(response))
   }, [])
+
+  const onSubmit: SubmitHandler<AddStockInputs> = async (data) => {
+    try {
+      await postTransaction({ ...data, transaction_type: 'harvest' })
+      navigate('/')
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return <div>Add stock form</div>
 }

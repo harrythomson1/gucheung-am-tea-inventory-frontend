a910b9e99b050ed0ada51da5868749ceb45010af
Harry Thomson <haroldt95@hotmail.com>
2026-03-18 17:53:13 +0900

    Space the login form correctly

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,10 +8,17 @@ export default function Login() {
   if (session) return <Navigate to="/dashboard" replace />
 
   return (
-    <div className="bg-[#f2f2e1] min-h-screen">
-      <div className="flex items-center justify-center min-h-screen">
+    <div className="bg-[#f2f2e1] min-h-screen flex flex-col">
+      <div className="flex-1 flex items-center justify-center">
+        <div>
+          <h1 className="text-center text-2xl font-bold">화엄구층제다</h1>
+          <h1 className="text-center text-2xl font-bold">관리 페이지</h1>
+        </div>
+      </div>
+      <div className="flex-1 flex items-center justify-center">
         <LoginForm />
       </div>
+      <div className="flex-1 flex items-center justify-center"></div>
     </div>
   )
 }

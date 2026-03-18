712b60080f4afd3f3959e068c7b866e953afe979
Harry Thomson <haroldt95@hotmail.com>
2026-03-18 11:14:06 +0900

    Translate flush labels on the add stock form

--- src/components/AddStockForm.tsx
+++ src/components/AddStockForm.tsx
@@ -7,7 +7,11 @@ import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import type { Tea } from '../types/tea'
 import type { Variant } from '../types/variant'
-import { PACKAGING_LABELS, TEA_NAMES } from '../constants/transalations'
+import {
+  FLUSH_LABELS,
+  PACKAGING_LABELS,
+  TEA_NAMES,
+} from '../constants/transalations'
 
 type AddStockInputs = Omit<AddTransactionData, 'transaction_type'>
 const PACKAGING_TYPES = ['silver', 'wing', 'gift', 'standard', 'mixed']
@@ -130,7 +134,7 @@ export function AddStockForm() {
                 setValue('flush', flush)
               }}
             >
-              {flush}
+              {FLUSH_LABELS[flush as keyof typeof FLUSH_LABELS]}
             </button>
           ))}
         </div>

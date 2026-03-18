f123d5e04425b40754b3a9f039362730530c18ad
Harry Thomson <haroldt95@hotmail.com>
2026-03-18 09:04:13 +0900

    Create an add tea method

--- src/api/tea.ts
+++ src/api/tea.ts
@@ -9,3 +9,8 @@ export const getTeaStock = async (teaId: number) => {
   const response = await api.get(`/teas/${teaId}/stock`)
   return response.data
 }
+
+export const addTea = async (teaName: string) => {
+  const response = await api.post('/teas', teaName)
+  return response.data
+}

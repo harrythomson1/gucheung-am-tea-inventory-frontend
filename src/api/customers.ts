c78fb6e8b319ed01e917ce91ae3810edc81987d2
Harry Thomson <haroldt95@hotmail.com>
2026-03-20 11:08:24 +0900

    Setup the modal to be able to add new customers

--- src/api/customers.ts
+++ src/api/customers.ts
@@ -1,8 +1,14 @@
+import type { CreateCustomerData } from '../types/customer'
 import api from './api'
 
 export const searchCustomers = async (search: string) => {
   const response = await api.get(
     `/customers?search=${encodeURIComponent(search)}`
   )
+  return response.data
+}
+
+export const createCustomer = async (customerData: CreateCustomerData) => {
+  const response = await api.post('/customers', customerData)
   return response.data
 }
--- src/components/AddCustomerModal.tsx
+++ src/components/AddCustomerModal.tsx
@@ -1,14 +1,55 @@
+import { useState } from 'react'
+import { createCustomer } from '../api/customers'
+
 type AddCustomerModalProps = {
   onClose: () => void
 }
 
 export function AddCustomerModal({ onClose }: AddCustomerModalProps) {
+  const [name, setName] = useState<string>('')
+  const [city, setCity] = useState<string>('')
+  const [address, setAddress] = useState<string>('')
+  const [phone, setPhone] = useState<string>('')
+  const [note, setNote] = useState<string>('')
+
+  const handleSubmit = async () => {
+    await createCustomer({
+      name,
+      city,
+      address: address || undefined,
+      phone: phone || undefined,
+      note: note || undefined,
+    })
+    onClose()
+  }
   return (
     <>
       <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
         <div className="bg-white p-6 rounded-lg w-80">
           <div>AddCustomerModal</div>
-          <button onClick={() => onClose()}>Submit</button>
+          <div>
+            <input
+              placeholder="Name"
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <input
+              placeholder="City"
+              onChange={(e) => setCity(e.target.value)}
+            ></input>
+            <input
+              placeholder="Address"
+              onChange={(e) => setAddress(e.target.value)}
+            ></input>
+            <input
+              placeholder="Phone"
+              onChange={(e) => setPhone(e.target.value)}
+            ></input>
+            <input
+              placeholder="Note"
+              onChange={(e) => setNote(e.target.value)}
+            ></input>
+          </div>
+          <button onClick={() => handleSubmit()}>Submit</button>
         </div>
       </div>
     </>

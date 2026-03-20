936a15d5e372a1ba4f53d973c85e4f0c5a3c25ac
Harry Thomson <haroldt95@hotmail.com>
2026-03-20 15:16:00 +0900

    Allow searching on the customer list page

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,19 +1,38 @@
 import { useEffect, useState } from 'react'
 import { getCustomers } from '../api/customers'
 import type { Customer } from '../types/customer'
+import { useNavigate } from 'react-router-dom'
 
 export function Customers() {
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [customers, setCustomers] = useState<Customer[]>([])
+  const [search, setSearch] = useState('')
+  const [debouncedSearch, setDebouncedSearch] = useState('')
   const skip = (currentPage - 1) * 20
+  const navigate = useNavigate()
 
   useEffect(() => {
-    getCustomers({ skip: skip }).then((response) => setCustomers(response))
-  }, [currentPage, skip])
+    const timer = setTimeout(() => setDebouncedSearch(search), 300)
+    return () => clearTimeout(timer)
+  }, [search])
+
+  useEffect(() => {
+    getCustomers({ skip, search: debouncedSearch || undefined }).then(
+      (response) => setCustomers(response)
+    )
+  }, [currentPage, skip, debouncedSearch])
+
   return (
     <div>
+      <input
+        placeholder="고객 검색"
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
       {customers.map((customer) => (
-        <div key={customer.id}>
+        <div
+          key={customer.id}
+          onClick={() => navigate(`/customers/${customer.id}`)}
+        >
           {customer.name} - {customer.city}
         </div>
       ))}

010c42056547abf6e1fc9c864d053e6ab09f75f6
Harry Thomson <haroldt95@hotmail.com>
2026-03-20 10:59:14 +0900

    Create a create customer data type

--- src/types/customer.ts
+++ src/types/customer.ts
@@ -6,3 +6,11 @@ export type Customer = {
   phone?: string
   notes?: string
 }
+
+export type CreateCustomerData = {
+  name: string
+  city: string
+  address?: string
+  phone?: string
+  notes?: string
+}

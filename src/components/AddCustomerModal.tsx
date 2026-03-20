2eb551f2f0b02f4f7c9b62073b32212e8021a6de
Harry Thomson <haroldt95@hotmail.com>
2026-03-20 10:42:04 +0900

    Style the add customer modal to work as an overlay

--- src/components/AddCustomerModal.tsx
+++ src/components/AddCustomerModal.tsx
@@ -5,8 +5,12 @@ type AddCustomerModalProps = {
 export function AddCustomerModal({ onClose }: AddCustomerModalProps) {
   return (
     <>
-      <div>AddCustomerModal</div>
-      <button onClick={() => onClose()}>Submit</button>
+      <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+        <div className="bg-white p-6 rounded-lg w-80">
+          <div>AddCustomerModal</div>
+          <button onClick={() => onClose()}>Submit</button>
+        </div>
+      </div>
     </>
   )
 }

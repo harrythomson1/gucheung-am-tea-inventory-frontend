7307c35efd8391857384934f77991440d2b2a1f9
Harry Thomson <haroldt95@hotmail.com>
2026-03-21 13:25:08 +0900

    Create UpdateCustomerModal using the edit customer modal as the template

---
+++ src/components/UpdateCustomerModal.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import { updateCustomer } from '../api/customers'
+import type { Customer, UpdateCustomerData } from '../types/customer'
+
+type UpdateCustomerModalProps = {
+  onClose: () => void
+  onCustomerUpdated: (customer: UpdateCustomerData) => void
+  customerId: number
+  currentCustomer: Customer
+}
+
+export function UpdateCustomerModal({
+  onClose,
+  onCustomerUpdated,
+  customerId,
+  currentCustomer,
+}: UpdateCustomerModalProps) {
+  const [name, setName] = useState<string>(currentCustomer.name)
+  const [city, setCity] = useState<string>(currentCustomer.city)
+  const [address, setAddress] = useState<string>(currentCustomer.address ?? '')
+  const [phone, setPhone] = useState<string>(currentCustomer.phone ?? '')
+
+  const handleSubmit = async () => {
+    const customer = await updateCustomer(customerId, {
+      name: name || undefined,
+      city: city || undefined,
+      address: address || undefined,
+      phone: phone || undefined,
+    })
+    onCustomerUpdated(customer)
+    onClose()
+  }
+  return (
+    <>
+      <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+        <div className="bg-white p-6 rounded-lg w-80">
+          <div>EditCustomerModal</div>
+          <div>
+            <input placeholder="Name" onChange={(e) => setName(e.target.value)}>
+              {currentCustomer.name}
+            </input>
+            <input placeholder="City" onChange={(e) => setCity(e.target.value)}>
+              {currentCustomer.city}
+            </input>
+            <input
+              placeholder="Address"
+              onChange={(e) => setAddress(e.target.value)}
+            >
+              {currentCustomer.address}
+            </input>
+            <input
+              placeholder="Phone"
+              onChange={(e) => setPhone(e.target.value)}
+            >
+              {currentCustomer.phone}
+            </input>
+          </div>
+          <button onClick={() => handleSubmit()} type="button">
+            Submit
+          </button>
+          <button onClick={() => onClose()} type="button">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}

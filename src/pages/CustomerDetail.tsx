d53631e638ae7dfeb5731ac96709342a5b12069d
Harry Thomson <haroldt95@hotmail.com>
2026-03-21 10:03:09 +0900

    Get customer name on customer details page

--- src/pages/CustomerDetail.tsx
+++ src/pages/CustomerDetail.tsx
@@ -1,3 +1,23 @@
+import { useState, useEffect } from 'react'
+import type { Customer } from '../types/customer'
+import { getCustomerWithId } from '../api/customers'
+import { useParams } from 'react-router-dom'
+
 export function CustomerDetail() {
-  return <div>Customer Detail</div>
+  const { customerId } = useParams<{ customerId: string }>()
+  const [customerData, setCustomerData] = useState<Customer | null>(null)
+
+  useEffect(() => {
+    getCustomerWithId(Number(customerId)).then((response) => {
+      setCustomerData(response)
+    })
+  }, [customerId])
+
+  if (!customerData) return <div>로딩 중...</div>
+
+  return (
+    <div>
+      <div>{customerData.name}</div>
+    </div>
+  )
 }

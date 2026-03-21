9cc707d379b67ad9e791c7a647137d1f7e6beef2
Harry Thomson <haroldt95@hotmail.com>
2026-03-21 14:41:27 +0900

    Display a list of current teas

--- src/pages/Teas.tsx
+++ src/pages/Teas.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react'
 import AddTeaModal from '../components/AddTeaModal'
+import type { Tea } from '../types/tea'
+import { getTeas } from '../api/tea'
 
 export default function Teas() {
-  return <AddTeaModal />
+  const [teas, setTeas] = useState<Tea[]>([])
+
+  useEffect(() => {
+    getTeas().then((response) => {
+      setTeas(response)
+    })
+  }, [])
+  return (
+    <>
+      {teas.map((tea) => (
+        <div key={tea.id}>{tea.name}</div>
+      ))}
+      <AddTeaModal />
+    </>
+  )
 }

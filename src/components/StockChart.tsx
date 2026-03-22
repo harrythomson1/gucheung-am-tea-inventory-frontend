09a1c59d286cf373457c421d8ab87a867656ee55
Harry Thomson <haroldt95@hotmail.com>
2026-03-22 15:34:56 +0900

    Centre the chart

--- src/components/StockChart.tsx
+++ src/components/StockChart.tsx
@@ -29,6 +29,7 @@ export function StockChart({ chartData, onBarClick }: StockChartProps) {
       <ResponsiveContainer width="100%" height={300}>
         <BarChart
           data={chartData}
+          margin={{ top: 0, right: 0, bottom: 0, left: -18 }}
           onClick={(data) => {
             if (onBarClick && data?.activeLabel) {
               const tea = chartData.find((t) => t.name === data.activeLabel)
